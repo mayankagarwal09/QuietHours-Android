4e5821f053155e9047e59279387a66f15330cbf6
mayankagarwal09 <mayankagarwal0909@gmail.com>
2019-04-01 14:57:06 +0530

    -version 1.3 project updated

--- app/src/main/java/com/mayanktechnologies/alarmmanager/AlarmReceiver.java
+++ app/src/main/java/com/mayanktechnologies/alarmmanager/AlarmReceiver.java
@@ -29,8 +29,6 @@ public class AlarmReceiver extends BroadcastReceiver{
             am.setStreamVolume(AudioManager.STREAM_MUSIC,Constants.CURRENT_MEDIA_VOLUME, AudioManager.ADJUST_MUTE);
             Constants.PHONE_RING_STATE=true;
             Log.v("alarmNoti","ring");
-            String text="Your phone has been set back to Ringer!";
-            sendNotification(context,text);
         }
 
         else if(action==Constants.ACTION_SILENT)
@@ -45,39 +43,9 @@ public class AlarmReceiver extends BroadcastReceiver{
             Constants.CURRENT_MEDIA_VOLUME=am.getStreamVolume(AudioManager.STREAM_MUSIC);
             am.setStreamVolume(AudioManager.STREAM_MUSIC,0,AudioManager.ADJUST_MUTE);
             Constants.PHONE_RING_STATE=false;
-            String text="Your phone has been put on Silent!";
-            sendNotification(context,text);
 
         }
 
-    }
-
-    public void sendNotification(Context context,String message) {
-
-        //Get an instance of NotificationManager//
-
-        NotificationCompat.Builder mBuilder =
-                (NotificationCompat.Builder) new NotificationCompat.Builder(context)
-                        .setSmallIcon(R.mipmap.ic_launcher)
-                        .setContentTitle("Quiet Hours")
-                        .setContentText(message);
-
-
-        // Gets an instance of the NotificationManager service//
-
-        NotificationManager mNotificationManager =
-
-                (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
-
-        // When you issue multiple notifications about the same type of event,
-        // it’s best practice for your app to try to update an existing notification
-        // with this new information, rather than immediately creating a new notification.
-        // If you want to update this notification at a later date, you need to assign it an ID.
-        // You can then use this ID whenever you issue a subsequent notification.
-        // If the previous notification is still visible, the system will update this existing notification,
-        // rather than create a new one. In this example, the notification’s ID is 001//
-
-                mNotificationManager.notify(001, mBuilder.build());
     }
 
 
